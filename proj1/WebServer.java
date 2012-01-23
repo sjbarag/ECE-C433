60187476e29f37030ecb473162d410eadf3e7b38
sjbarag <sjbarag@gmail.com>
2012-01-23 11:55:28 -0500

    Add status code words back in

--- proj1/WebServer.java
+++ proj1/WebServer.java
@@ -113,12 +113,12 @@ final class HttpRequest implements Runnable
 
 		if( fileExists )
 		{
-			statusLine = "200" + CRLF;
+			statusLine = "200 OK" + CRLF;
 			contentTypeLine = "Content-type: " + contentType( filename ) + CRLF;
 		}
 		else
 		{
-			statusLine = "404" + CRLF;
+			statusLine = "404 Not Found" + CRLF;
 			contentTypeLine = "Content-type: text/html" + CRLF;
 			entityBody = "<HTML>" +
 				"<HEAD><TITLE>Not Found</TITLE></HEAD>" +
