b7182e0c3c16349b3b1ef002d5150efa0592250a
sjbarag <sjbarag@gmail.com>
2012-01-30 21:37:52 -0500

    Add part 1b
    IP address prints correctly but netmask doesn't.  Wat do?

---
+++ proj2/p1b.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <pcap/pcap.h>
+
+void main()
+{
+	/* check for root uid :) */
+	if( getuid() != 0 )
+	{
+		fprintf(stderr, "Error: You must this application as root, either via sudo or su.\nExiting...\n");
+		exit(-1);
+	}
+
+	/* return for various functions */
+	int status = 0;
+
+	/* error buffer */
+	char errbuf[PCAP_ERRBUF_SIZE];
+
+	/* linked list of all devices that can be opened with pcap_open_live and
+	 * friends */
+	pcap_if_t *all_devs;
+
+	/* iterator for all_devs.  I want to maintain that list's HEAD */
+	pcap_if_t *devs_it;
+
+	/* temporary storage for an interface's address info */
+	pcap_addr_t *temp_addr;
+
+	char *addr;
+	char *nm;
+
+	/* populate all_devs */
+	status = pcap_findalldevs(&all_devs, errbuf);
+
+	if( status != 0 )
+	{
+		fprintf(stderr,
+		        "pcap_findalldevs() failed.  The following error was produced:\n\t%s\n",
+		        errbuf);
+		exit(-1);
+	}
+	else if( all_devs == NULL )
+	{
+		printf("No devices were found.  Bummer!\n");
+	}
+	else
+	{
+		devs_it = all_devs;
+		printf("Name\tIP address\tNetmask\n------\t---------------\t---------------\n");
+		while( devs_it != NULL )
+		{
+			temp_addr = devs_it->addresses;
+			while( temp_addr != NULL )
+			{
+				if( temp_addr->netmask != NULL )
+				{
+					/* check for IPv4 */
+					if( temp_addr->addr->sa_family == AF_INET )
+					{
+						printf("%6s\t%15s\t%15d\n",
+						       devs_it->name,
+						       inet_ntoa( ( (struct sockaddr_in *)temp_addr->addr)->sin_addr ),
+						       inet_ntoa( ( (struct sockaddr_in *)temp_addr->netmask)->sin_addr )
+							   );
+					}
+				}
+				temp_addr = temp_addr->next;
+			}
+			devs_it = devs_it->next;
+		}
+	}
+
+	/* free the list of devices */
+	pcap_freealldevs(all_devs);
+}
