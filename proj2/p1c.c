6ebb16d0a26fe26b653d0cdc70b35b0d30e6c938
sjbarag <sjbarag@gmail.com>
2012-01-31 22:17:19 -0500

    Add thread to cancel sniff loop
    Doesn't count anything yet

--- proj2/p1c.c
+++ proj2/p1c.c
@@ -8,8 +8,14 @@
 #include <pcap/pcap.h>
 #include <pthread.h>
 
-/* network descriptor */
-pcap_t *nd;
+#define _REENTRANT        /* for pthreads! */
+
+/* arguments for the sniff_timer thread */
+typedef struct thread_args
+{
+	unsigned int time;    /* time to sleep in seconds */
+	pcap_t *nd;           /* network descriptor */
+} THREAD_ARGS;
 
 
 void proc_pkt(u_char *user, const struct pcap_pkthdr *h, const u_char *bytes)
@@ -17,44 +23,49 @@ void proc_pkt(u_char *user, const struct pcap_pkthdr *h, const u_char *bytes)
 	printf("Got a packet!\n");
 }
 
-/*void sniff_timer(void *args)
+void *sniff_timer(void *in_args)
 {
+	THREAD_ARGS *l_args = (THREAD_ARGS *) in_args;
 	printf("D/sniff_timer: Starting timer...\n");
-	sleep( atoi(argv[1]) );
+	sleep( l_args->time );
 
 	printf("D/sniff_timer: Canceling loop!\n");
-}*/
+	pcap_breakloop( l_args->nd );
+}
 
 
 
-void main( int argc, char* argv )
+void main( int argc, char** argv )
 {
 	/* check for root uid :) */
 	if( getuid() != 0 )
 	{
-		fprintf(stderr, "Error: You must this application as root, either via sudo or su.\nExiting...\n");
+		fprintf(stderr,
+				"Error: You must this application as root, either via sudo %s",
+				"or su.\nExiting...\n");
 		exit(-1);
 	}
 
-	/* check argc */
-	if( argc != 2 )
-	{
-		fprintf(stderr, "Usage: ./p1c <number of seconds to sniff>\nExiting...\n");
-		exit(-1);
-	}
-
+	/* --- misc --- */
+	int sleep_time = (argc == 2) ? atoi(argv[1]) : 5; /* time to sniff */
+	printf("D/main: Sleep time = %d\n", sleep_time);
 	int status = 0;                   /* return for various functions */
-	char errbuf[PCAP_ERRBUF_SIZE];    /* error buffer */
 	int got_device = 0;               /* flag */
 
+	/* --- pcap specific stuff --- */
 	pcap_if_t *all_devs;              /* linked list of all devices that can be opened
 	                                   *   with pcap_open_live and friends */
 	pcap_if_t *devs_it;               /* iterator for all_devs, to presesrve
 	                                   *   all_devs' HEAD */
 	pcap_addr_t *temp_addr;           /* temporary storage for an interface's
 	                                   *   address info */
-
+	pcap_t *nd;                       /* network descriptor */
 	char *device = (char *)malloc(sizeof(char)*8); /* device name.  8 chars is arbitrary */
+	char errbuf[PCAP_ERRBUF_SIZE];    /* error buffer */
+
+	/* --- pthread stuff --- */
+	pthread_t timer;                  /* threadID for the timer */
+	THREAD_ARGS *t_args;              /* thread arguments structure */
 
 
 	/* ----- populate all_devs ----- */
@@ -115,8 +126,24 @@ void main( int argc, char* argv )
 	nd = pcap_open_live(device, BUFSIZ, 0, 2, errbuf);
 	printf("Using device:\t%s\n", device);
 
+	/* ----- fill thread args ----- */
+	t_args = (THREAD_ARGS *)malloc(sizeof(THREAD_ARGS));
+	t_args->time = sleep_time;
+	t_args->nd   = nd;
+
+	/* ----- spawn thread ----- */
+	if( (pthread_create( &timer, NULL, sniff_timer, (void *)t_args)) != 0)
+	{
+		fprintf(stderr, "Error: could not create thread!\nExiting...\n");
+		exit(-1);
+	}
+
 	/* ----- start loop ----- */
 	status = pcap_loop( nd, 0, proc_pkt, NULL );
+
+	/* ----- wait for thread to rejoin ----- */
+	pthread_join( timer, NULL );
+	printf("D/main: timer thread joined successfully!\n");
 
 	if( status == 0 )
 		printf("count exhausted");
@@ -124,5 +151,4 @@ void main( int argc, char* argv )
 		fprintf(stderr, "pcap_loop threw error:\n\t%s\n", errbuf);
 	else if( status == -2 )
 		fprintf(stderr, "Loop terminated by pcap_breakloop\n");
-
 }
