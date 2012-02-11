06d93479d8a59458fd0f89f2aea9812034ef2df3
sjbarag <sjbarag@gmail.com>
2012-02-11 09:09:12 -0500

    Process packet size data

--- proj2/procData.py
+++ proj2/procData.py
@@ -3,44 +3,68 @@
 # Author: Sean Barag <sjb89@drexel.edu>
 
 # read data
-data = []
+tdata = []
+sdata = []
 f = open("timing_data.txt", "r")
 for line in f:
-	data.append(float(line.strip()))
+	tdata.append(float(line.split()[0]))
+	sdata.append(int(line.split()[1]))
 f.close()
 
-# normalize data to time since first packet
-init = data[0]
-data = [ (val - init) for val in data ]
+# ---------- timing data ----------
 
+# normalize tdata to time since first packet
+init = tdata[0]
+tdata = [ (val - init) for val in tdata ]
 
-# floor data
-mfloor = [int(val // 60) for val in data]
-sfloor = [int(val // 1) for val in data]
-ufloor = [int(val // 0.001) for val in data]
+# floor tdata
+mfloor = [int(val // 60) for val in tdata]
+sfloor = [int(val // 1) for val in tdata]
+ufloor = [int(val // 0.001) for val in tdata]
 floored = [ mfloor, sfloor, ufloor ]
 
-# bin data
-vals = [ [],[],[] ]
-counts = [ [],[],[] ]
+# bin tdata
+tvals = [ [],[],[] ]
+tcounts = [ [],[],[] ]
 for bin_i in range(3):
 	for val in floored[bin_i]:
-		if val in vals[bin_i]:
-			counts[bin_i][ vals[bin_i].index(val) ] += 1
+		if val in tvals[bin_i]:
+			tcounts[bin_i][ tvals[bin_i].index(val) ] += 1
 		else:
-			vals[bin_i].append(val)
-			counts[bin_i].append(1)
+			tvals[bin_i].append(val)
+			tcounts[bin_i].append(1)
 
-# print to file
-f = []
+# open tfiles
+f = [] # array of file descriptors
 f.append( open("binned_data_m.txt", "w") )
 f.append( open("binned_data_s.txt", "w") )
-f.append( open("binned_data_us.txt", "w") )
+f.append( open("binned_data_ms.txt", "w") )
 
-for i in range(len(vals)):
-	for v,c in zip(vals[i], counts[i]):
+# write tdata
+for i in range(len(tvals)):
+	for v,c in zip(tvals[i], tcounts[i]):
 		f[i].write( '{:d}{:s}{:d}{:s}'.format(v, '\t\t', c, '\n') )
-		#print v, "\t\t", c
 
+#close tfiles
 for i in range(len(f)):
 	f[i].close()
+
+# ---------- size data ----------
+# bin sdata
+scounts = [ [], [] ]
+for size in sdata:
+	if size in scounts[0]:
+		scounts[1][ scounts[0].index(size) ] += 1
+	else:
+		scounts[0].append(size)
+		scounts[1].append(1)
+
+# calculate frequencies
+pktcount = len(sdata)
+scounts[1] = [ float(c)/float(pktcount) for c in scounts[1] ]
+
+# write sdata
+f = open("frequency_data.txt", "w")
+for v,c in zip(scounts[0], scounts[1]):
+	f.write( '{:d}{:s}{:f}{:s}'.format(v, '\t\t', c, '\n') )
+f.close()
