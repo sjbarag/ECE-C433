de0dcdad409f16a39b552d5b82679f7cd63c917c
Sean Barag <sjbarag@gmail.com>
2012-02-29 11:17:25 -0500

    Initial commit

---
+++ proj4/incrementClient.c
@@ -0,0 +1,105 @@
+//Originally code by Dr.Harish Sethu. ECE-C433. Spring 2006-2007
+//to run: gcc incrementClient.c -lnsl -lsocket -o incrementClient
+//Accepts as parameter the IP address of the server and the number to be incremented
+
+
+#include <stdio.h>           /* needed for file io */
+#include <sys/socket.h>      /* needed for socket(), bind() and connect() */
+#include <arpa/inet.h>       /* needed for socket structures and byte-order operations */
+#include <stdlib.h>          /* needed for several useful functions such as atoi() */
+#include <string.h>          /* needed for memset and related string functions */
+#include <unistd.h>          /* needed for close() */
+
+#define SERVER_PORT 10001
+#define RECVBUFSIZE 64
+
+int main( int argc, char *argv[] )
+{
+   /* Socket descriptor and address */
+   int sockfd;               
+   struct sockaddr_in serverAddr; 
+
+   /* The string of digits transmitted to server */
+   char *sendNumberString;   
+
+   /* number of bytes transmitted to server */
+   unsigned int sendNumberStringLen; 
+
+   /* buffer in which incremented number string is stored */
+   char recvBuffer[RECVBUFSIZE];
+   /* number of bytes received from the server */
+   int numBytesRcvd = 0;
+   /* character used to store each byte received from server */
+   char c = ' ';
+
+
+   if ( argc != 3 )
+      {
+         fprintf( stderr, "usage: incrementClient <ServerIPaddress> <number>\n" );
+         exit( 0 );
+      }
+
+   /* create socket */
+   sockfd = socket( AF_INET, SOCK_STREAM, IPPROTO_TCP );
+   if ( sockfd < 0 )
+      {
+         perror( "socket error" );
+         exit( 1 );
+      }
+   printf( "Created socket.\n" );
+
+   /* zero out the socket address */
+   bzero( &serverAddr, sizeof( serverAddr ) );
+
+   /* set socket address to server address and port */
+   serverAddr.sin_family = AF_INET;
+   serverAddr.sin_port = htons( SERVER_PORT );
+   if ( inet_pton( AF_INET, argv[ 1 ], &serverAddr.sin_addr ) <= 0 )
+      {
+         perror( "inet_pton() failed" );
+         exit( 0 );
+      }
+
+   /* connect to server */
+   if ( connect( sockfd, (struct sockaddr *) &serverAddr, sizeof( serverAddr ) ) < 0 )
+      {
+         perror( "connect error" );
+         exit( 1 );
+      }
+   printf( "Connected to server.\n" );
+
+   /* set number to send */
+   sendNumberString = argv[ 2 ];
+
+   /* number of bytes to send is 1 more than the number of digits in the number
+      because we have to also send the delimiter character '\0' */
+   sendNumberStringLen = strlen( sendNumberString ) + 1;
+   
+   if ( write( sockfd, sendNumberString, sendNumberStringLen ) != sendNumberStringLen )
+      {
+         perror( "write error" );
+         exit( 1 );
+      }
+   printf( "Sent number %s to server.\n", sendNumberString );
+
+   /* receive characters from server one by one until you finally receive
+      the delimiter character */
+   while ( c != '\0' )
+      {
+         if ( read( sockfd, &c, 1 ) == 1 )
+            {
+               recvBuffer[ numBytesRcvd ] = c;
+               numBytesRcvd++;
+            }
+         else
+            {
+               perror( "read() error" );
+               exit( 1 );
+            }
+      }
+   printf( "Received incremented number %s from server.\n", recvBuffer );
+
+   /* close socket and exit */
+   close( sockfd );
+   exit( 0 );
+}
---
+++ proj4/incrementServer.c
@@ -0,0 +1,132 @@
+//Originally code by Dr.Harish Sethu. ECE-C433. Spring 2006-2007
+//to run: gcc incrementServer.c -lnsl -lsocket -o incrementServer
+//Takes no parameters
+
+
+
+#include <stdio.h>           /* needed for file io */
+#include <sys/socket.h>      /* needed for socket(), bind() and connect() */
+#include <arpa/inet.h>       /* needed for socket structures and byte-order operations */
+#include <stdlib.h>          /* needed for several useful functions such as atoi() */
+#include <string.h>          /* needed for memset and related string functions */
+#include <unistd.h>          /* needed for close() */
+
+#define BACKLOG 5
+#define SERVER_PORT 10001
+#define RECVBUFSIZE 64
+
+int main( int argc, char *argv[] )
+{
+
+   /* client and server sockets and addresses */
+   int serverSocket;
+   int clientSocket;
+   struct sockaddr_in serverAddr;
+   struct sockaddr_in clientAddr;
+   unsigned int clientAddrLen;
+
+   /* buffer into which number received from client is stored */
+   char recvBuffer[RECVBUFSIZE];
+   /* number of bytes received from the client */
+   int numBytesRcvd;
+   /* character used to store each byte received from client */
+   char c;
+
+   /* buffer in which the incremented number is stored */
+   char incNumberString[RECVBUFSIZE];
+
+   /* number of bytes to send to client in order to send the incremented number */
+   unsigned int incNumberStringLen;
+
+   if ( argc != 1 )
+      {
+         fprintf( stderr, "usage: no arguments\n" );
+         exit( 0 );
+      }
+
+   /* create server side socket */
+   serverSocket = socket( PF_INET, SOCK_STREAM, IPPROTO_TCP );
+   
+   if ( serverSocket < 0 )
+      {
+         perror( "socket error" );
+         exit( 1 );
+      }
+   printf( "Socket created.\n" );
+
+   /* zer out socket address */
+   bzero( &serverAddr, sizeof( serverAddr ) );
+
+   /* set socket address to server address and port. Note that the constant
+      INADDR_ANY implies that the client may reach the server through any 
+      incoming interface. */
+   serverAddr.sin_family = AF_INET;
+   serverAddr.sin_addr.s_addr = htonl( INADDR_ANY );
+   serverAddr.sin_port = htons( SERVER_PORT );
+
+   /* bind server address and port to server side socket */
+   if ( bind( serverSocket, (struct sockaddr *) &serverAddr, sizeof( serverAddr ) ) < 0 )
+      {
+         perror( "bind() error" );
+         exit( 1 );
+      }
+   printf( "Address binding successful.\n" );
+
+   /* call listen on the server side socket */
+   if ( listen( serverSocket, BACKLOG ) < 0 )
+      {
+         perror( "listen() error" );
+         exit( 1 );
+      }
+   printf( "Server process is listening for connection requests.\n" );
+
+   /* Now, prepare to accept connection requests from clients */
+   clientAddrLen = sizeof( clientAddr );
+
+   /* You run the for loop forever since as soon as the server serves a client,
+      it should get ready to serve another client. */
+   for (;;)
+      {
+         clientSocket = accept( serverSocket, (struct sockaddr *) &clientAddr, &clientAddrLen );
+         if ( clientSocket < 0 )
+            {
+               perror( "accept() error" );
+               exit( 1 );
+            }
+
+         /* Begin receiving bytes from the client */
+         numBytesRcvd = 0;
+         c = ' ';
+         while ( c != '\0' )
+            {
+               if ( read( clientSocket, &c, 1 ) == 1 )
+                  {
+                     recvBuffer[ numBytesRcvd ] = c;
+                     numBytesRcvd++;
+                  }
+               else
+                  {
+                     perror( "read() error" );
+                     exit( 1 );
+                  }
+            }
+         printf( "Received number %s from a client.\n", recvBuffer );
+
+         /* increment received number and store it as a string */
+         sprintf( incNumberString, "%d", atoi( recvBuffer ) + 1 );
+         
+         /* number of bytes to send to client is one more than the number of 
+            digits in the incremented number since we also count the delimiter '\0' */
+         incNumberStringLen = strlen( incNumberString ) + 1;
+
+         /* send incremented number to client */
+         if ( write( clientSocket, incNumberString, incNumberStringLen ) != incNumberStringLen )
+            {
+               perror( "send error" );
+               exit( 1 );
+            }
+         printf( "Returned number %s to the client.\n", incNumberString );
+         
+         close( clientSocket );
+      }
+}
